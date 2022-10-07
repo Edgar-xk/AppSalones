9c7f352b706ada3cf8bb319043ae39d85b07b17e
Edgar Sotelo <edgar.sotelo.arceo@gmail.com>
2022-10-07 16:17:57 -0500

    Se inicia la App

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import {ReservarService} from '../services/reservar/reservar.service'
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,85 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  FechaSeleccionada: Date;
+  HoraInicio;
+  HoraFin;
+  fecha: Date
+  fechaFormateadaI: string;
+  fechaFormateadaF: string;
+  constructor(private alertController: AlertController,reservarService:ReservarService) {
+    this.fecha = new Date();
+    console.log((this.fecha.getHours() + 1) + ':' + this.fecha.getMinutes());
 
+
+    this.fechaFormateadaI = "2022-10-07T" + (this.fecha.getHours()<10?"0"+this.fecha.getHours():this.fecha.getHours()) + ":" + (this.fecha.getMinutes()<10?"0"+this.fecha.getMinutes():this.fecha.getMinutes()) + ":00-23:00";
+    this.fechaFormateadaF = "2022-10-07T" + (this.fecha.getHours()+1<10?"0"+this.fecha.getHours():this.fecha.getHours()) + ":" + (this.fecha.getMinutes()<10?"0"+this.fecha.getMinutes():this.fecha.getMinutes()) + ":00-23:00";
+
+  }
+  ngOnInit() {
+
+  }
+
+
+
+  async InfoCodigo() {
+    await this.VerificarOcupacion();
+    const alert = await this.alertController.create({
+      header: 'Codigo: 51518',
+      message: "Guarde este codigo para realizar modificaciones",
+      buttons: [
+
+        {
+          text: 'OK',
+          role: 'confirm'
+
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+
+  }
+
+  async VerificarOcupacion() {
+    if ((<HTMLInputElement>document.getElementById("fecha")).value == undefined) {
+      const alert = await this.alertController.create({
+        header: 'Error ',
+        message: "Seleccione fecha",
+        buttons: [
+
+          {
+            text: 'OK',
+            role: 'confirm'
+
+          },
+        ],
+      });
+
+      await alert.present();
+
+      const { role } = await alert.onDidDismiss();
+    } else {
+
+
+      //llamar servicio de consultar datos
+      
+
+
+
+
+
+
+      
+
+      console.log((<HTMLInputElement>document.getElementById("fecha")).value);
+      console.log((<HTMLInputElement>document.getElementById("datetime")).value);
+
+    }
+
+
+
+  }
 }
